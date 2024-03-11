a4c1adba748071a587cbce9a4f32cb057a505f15
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-03-11 21:11:16 +0300

    Ibraah-MD

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ global.mentionaudio = mm.split(",");
 global.owner = oo.split(",");
 module.exports = {
   mongodb: process.env.MONGODB_URL || "",
-  BOT_INFO: process.env.BOT_INFO || "X-BOT-MD;Aswin Sparky;https://graph.org/file/9b477aaa6b90bbb46e4be.mp4",
-  STICKER_DATA: process.env.STICKER_DATA || "X-BOTMD;ASWIN SPARKY",
+  BOT_INFO: process.env.BOT_INFO || "IBRAAH-MD;Ibrahim-tech;https://telegra.ph/file/9bc81b0f606ee3fd374db.jpg",
+  STICKER_DATA: process.env.STICKER_DATA || "Tech;Ibraah",
   SESSION_ID: process.env.SESSION_ID || "",
   MENTION: process.env.MENTION || "true",
   MENTION_DATA: process.env.MENTION_DATA || 'X-BOT-MD;MULTI-DEVICE BOT BY TEAM EX-BOT-Z;https://wa.me/917012984396;https://i.imgur.com/3cMVKBk.jpeg',
